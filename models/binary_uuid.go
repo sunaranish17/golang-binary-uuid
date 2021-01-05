25637144d818fd8c8e8c7736e536e1e6c11611c1
Anish <sunaranish17@gmail.com>
2021-01-05 13:34:05 +0545

    binary_uuid completed

--- models/binary_uuid.go
+++ models/binary_uuid.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+
 	"github.com/google/uuid"
 )
 
@@ -16,4 +20,33 @@ func (b BinaryUUID) MarshalJSON() ([]byte, error) {
 	s := uuid.UUID(b)
 	str := "\"" + s.String() + "\""
 	return []byte(str), nil
+}
+
+//UnmarshalJSON -> convert from json to string
+func (b *BinaryUUID) UnmarshalJSON(by []byte) error {
+	s, err := uuid.ParseBytes(by)
+	*b = BinaryUUID(s)
+	return err
+}
+
+//GormDataType -> sql datatype for gorm
+func (BinaryUUID) GormDataType() string {
+	return "binary(16)"
+}
+
+//Scan -> scan value into BinaryUUID
+func (b *BinaryUUID) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSON value:", value))
+	}
+
+	data, err := uuid.FromBytes(bytes)
+	*b = BinaryUUID(data)
+	return err
+}
+
+//Value -> return BinaryUUID to []bytes binary(16)
+func (b BinaryUUID) Value() (driver.Value, error) {
+	return uuid.UUID(b).MarshalBinary()
 }
